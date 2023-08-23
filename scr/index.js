865b7703996eeb3ba7d61abc08daef3ebfdf9bc2
itcodegirl@gmail.com <88835851+itcodegirl@users.noreply.github.com>
2023-08-23 18:03:02 -0500

    Initial commit

---
+++ scr/index.js
@@ -0,0 +1,24 @@
+//JavaScript Objects vs Arrays</h1>
+
+//JS Challenge 1
+//Given an object called paris, log its temperature
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true
+};
+
+console.log(paris[3]);
+//JS Challenge 2
+//Create an object called sydney similar to paris and log its temperature
+//(use fake data)
+
+//JS Challenge 3
+//Create an array with 3 different Australian
+//city names only and log each name
+
+//JS Challenge 4
+//Create an array of 3 cities with the same properties as paris and console
+//log each city temperature
